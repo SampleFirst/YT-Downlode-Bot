d0e42e6ef635a4128c91a22ab6c469e037c71126
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-17 16:41:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # API credentials
-API_ID = 'your_api_id'
-API_HASH = 'your_api_hash'
+API_ID = '23906038'
+API_HASH = 'dff1eb42fad7971f16da662a99c0f376'
 
 # Bot token
-BOT_TOKEN = 'your_bot_token'
+BOT_TOKEN = '5891308865:AAHK-iw8PiSLOHEDF6JqmVRiQmj43uYXFnA'
 
 # Directory for storing downloaded files
 DOWNLOADS_DIR = 'downloads'
