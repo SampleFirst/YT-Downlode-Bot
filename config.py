8c42abfc291bf344c9ad60d64f5f9d93dec14968
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-17 17:14:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
+import os
+
+class Config(object):
+  
 # API credentials
-API_ID = '23906038'
-API_HASH = 'dff1eb42fad7971f16da662a99c0f376'
+API_ID = int(os.environ.get("API_ID", 23906038))
+API_HASH = os.environ.get("API_HASH" "dff1eb42fad7971f16da662a99c0f376")
 
 # Bot token
-BOT_TOKEN = '5891308865:AAHK-iw8PiSLOHEDF6JqmVRiQmj43uYXFnA'
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5891308865:AAHK-iw8PiSLOHEDF6JqmVRiQmj43uYXFnA")
 
 # Directory for storing downloaded files
 DOWNLOADS_DIR = 'downloads'
